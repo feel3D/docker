043e01307cd8251cbcfd5521007b71a8778a33d9
Lev Zhankobilov <feel3d@yandex.ru>
2023-02-27 02:33:33 +0300

    nginx+php+mysql

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo 'Hello';
+echo 'Hello';
+echo 'Hello';
+echo 'Hello';
+echo 'Hello';
+echo 'Hello';
+echo 'Hello';
+
+$c = mysqli_connect('mysql', 'root', 'root');
+
+$c->query('CREATE DATABASE `docker-db`');
+
+
