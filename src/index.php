de52043c029d753d9a734657b483e9fa4828269a
Lev Zhankobilov <feel3d@yandex.ru>
2023-03-10 01:34:41 +0300

    create table, test mysql volume, add readme

--- src/index.php
+++ src/index.php
@@ -8,8 +8,14 @@ echo 'Hello';
 echo 'Hello';
 echo 'Hello';
 
-$c = mysqli_connect('mysql', 'root', 'root');
+$c = mysqli_connect('mysql', 'root', 'root', 'docker-db');
 
-$c->query('CREATE DATABASE `docker-db`');
+//$c->query('CREATE DATABASE `docker-db`');
+//$sql = 'INSERT INTO users (name) VALUES ("Dal")';
+//$result = mysqli_query($c, $sql);
 
+$result = mysqli_query($c, 'SELECT * FROM users');
+while ($row = mysqli_fetch_array($result)) {
+    var_dump($row['name']);
+}
 
