f707898d4f611ca58717e86e419bd93df3204d8a
Lev Zhankobilov <feel3d@yandex.ru>
2023-03-12 01:43:31 +0300

    add mysql ports, make file

--- src/index.php
+++ src/index.php
@@ -1,21 +1,16 @@
 <?php
 
 echo 'Hello';
-echo 'Hello';
-echo 'Hello';
-echo 'Hello';
-echo 'Hello';
-echo 'Hello';
-echo 'Hello';
 
-$c = mysqli_connect('mysql', 'root', 'root', 'docker-db');
+
+$c = mysqli_connect('mysql', 'root', 'root', 'docker');
 
 //$c->query('CREATE DATABASE `docker-db`');
 //$sql = 'INSERT INTO users (name) VALUES ("Dal")';
 //$result = mysqli_query($c, $sql);
 
-$result = mysqli_query($c, 'SELECT * FROM users');
-while ($row = mysqli_fetch_array($result)) {
-    var_dump($row['name']);
-}
+//$result = mysqli_query($c, 'SELECT * FROM users');
+//while ($row = mysqli_fetch_array($result)) {
+//    var_dump($row['name']);
+//}
 
